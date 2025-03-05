4c120de09f30dde454b2c0ae833d64fca134efb5
Merry-robot <elijahjubrey@gmail.com>
2025-03-05 08:38:23 -0500

    actually add callback

---
+++ src/routes/callback/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import { onMount } from 'svelte';
+    import { goto } from '$app/navigation';
+    import { isAuthenticated } from '$lib/stores/auth.js';
+
+    onMount(() => {
+        // Simulate handling the auth response
+        setTimeout(() => {
+            isAuthenticated.set(true);
+            goto('/'); // Redirect to homepage
+        }, 1000);
+    });
+</script>
+
+<p>Logging you in...</p>
